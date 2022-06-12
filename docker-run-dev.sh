e4ea6bf549ab148cc60bcff67da3bc197d4c789b
Anton Chernous <a.chernous@notamedia.ru>
2022-06-12 21:52:10 +0300

    add frontend

--- docker-run-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker rm -f fs-freelance-app-api
-docker-compose -f docker-compose.yml -f docker-compose.development.yml up --build -d 
-echo -e "\033[33mremoving dangling images"
-docker image prune -f 
--- docker-run-prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose up --build -d
-echo -e "\033[33mremoving dangling images"
-docker image prune -f 
